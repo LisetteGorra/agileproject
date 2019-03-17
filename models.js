9c4a41a97d4b236bdc57a8a40f61947043ee25e8
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-17 21:54:35 +0000

    Created object model/Schema

---
+++ models.js
@@ -0,0 +1,13 @@
+{
+    identification: 'Number';
+    departmant: 'String';
+    name: 'String';
+    salary: 'Number';
+    position: 'String';
+    shift: 'String'
+    temp: true;
+    perminent: false;
+    salary: true;
+    hourly: false;
+    
+}
