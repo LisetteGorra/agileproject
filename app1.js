3356ab817b965665cd26dbf56f46b0596fd7ea08
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-18 22:56:19 +0000

    Created express app and connected to mongod with mongoose.connect

--- app1.js
+++ app1.js
@@ -1,6 +1,15 @@
-var express = require('express');
-var mongoose = require('mongoose');
-var bodyParser = require('body-parser');
-var ejs = require('ejs');
+const express = require("express");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const ejs = require('ejs');
+
+
+//set up express app 
+const app= express();
+
+//connect to mongod
+mongoose.connect("mongod://localhost/employees", {useNewUrlParser:true});
+mongoose.Promise = global.Promise; // use this because mongoose is deprecated 
+
 
 
