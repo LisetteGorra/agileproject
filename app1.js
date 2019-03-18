b596032cf21a41de16fc6a445e154debd652a3ad
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-18 23:14:21 +0000

    created app for body parser, ejs, and express

--- app1.js
+++ app1.js
@@ -11,5 +11,14 @@ const app= express();
 mongoose.connect("mongod://localhost/employees", {useNewUrlParser:true});
 mongoose.Promise = global.Promise; // use this because mongoose is deprecated 
 
+//To serve static files such as images, CSS files and JavaScript files, use the express.static built in middleware function in Express
+app.use(express.static(__dirname + '/public'));
 
+//Set the app to use ejs files 
+app.set("view engine", "ejs");
 
+//You need to use bodyParser() if you want the form data to be available in req.body 
+app.use(bodyParser.json());
+
+// When we need to initialize and use routing, use this. Remove the <> symbols and add your directory name 
+// app.use ("/<<directory name here>>", require("./routes/<<directory name here>>"));
