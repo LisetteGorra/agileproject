7f0aca57bcdbb10611c0a923739cc165d6af3232
Lisette Gorra <lisettegorra.student@careerdevs.com>
2019-03-20 22:37:40 +0000

    Adding get and post requests

--- index.js
+++ index.js
@@ -5,9 +5,18 @@ const express = require('express');
 //set up express app
 const app = express();
 
+app.get('/api', function(req, res){
+    console.log('GET request');   
+    res.send({name: 'Lisette'});
+});
+
+app.post('/api', function(req, res){
+    console.log('POST request');   
+    res.send({name: 'Lisette'});
+});
 
 //listen for requests 
-app.listen(4000, function(){
-   
-    
+app.listen(process.env.PORT, process.env.IP, function(){
+   console.log("Now listening for requests");
 });
+
